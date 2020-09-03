c47d77f598123e25807115c06ff1ce2bde4b63bd
Devin Cooper <dcoope12@asu.edu>
2020-09-03 01:17:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,14 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elseif int % 5 == 0
+  "Buzz"
+elseif int % 3 == 0 && int % 5 == 0
+"FizzBuzz"
+else 
+  "nil"
+  end
 end
